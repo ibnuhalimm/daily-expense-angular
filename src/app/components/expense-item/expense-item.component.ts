b134c1705448baa882d79874ce2a1e83b2285f73
Ibnu Halim Mustofa <ibnuhalim@pm.me>
2022-08-22 10:50:30 +0700

    refactor component

--- src/app/components/expense-item/expense-item.component.ts
+++ src/app/components/expense-item/expense-item.component.ts
@@ -1,8 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector: 'app-expense-item',
     templateUrl: './expense-item.component.html'
 })
 
-export class ExpenseItemComponent {}
+export class ExpenseItemComponent {
+    @Output() onClick: EventEmitter<any> = new EventEmitter();
+
+    onClickEvent() {
+        this.onClick.emit();
+    }
+}
