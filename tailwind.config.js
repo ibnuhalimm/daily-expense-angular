8833978b545f52eb1b8a2bc6e3ffa5b8edf32a55
Ibnu Halim Mustofa <ibnuhalim@pm.me>
2022-08-26 18:53:50 +0700

    extend color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./src/**/*.{html,ts}"
-    ],
-    theme: {
-        extend: {
-            fontSize: {
-                'xxs': '.6rem'
-            }
-        },
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        'xxs': '.6rem'
+      },
+      colors: {
+        'charged-blue': '#1DACE3',
+        'cerise-pink': '#E9327C',
+        'oxford-blue': '#001A49',
+        'chinese-silver': '#CCCCCC',
+        'cultured': '#F5F5F5',
+        'ghost-white': '#F9F9F9'
+      }
     },
-    plugins: [],
+  },
+  plugins: [],
 }
